8e377402f9bedfd23adc190b4a0cc94e5374139b
Piotrek <p.sztefko@gmail.com>
2021-06-15 15:37:20 +0200

    Add project files.

---
+++ AddressBook/Models/AddressContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models {
+	public class AddressContext : DbContext {
+
+		public AddressContext(DbContextOptions<AddressContext> options) : base(options) {
+
+			Database.EnsureCreated();
+		}
+
+		public DbSet<Address> Addresses { get; set; }
+
+	}
+}
