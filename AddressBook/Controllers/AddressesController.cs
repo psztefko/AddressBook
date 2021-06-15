7c2d9175f6a5b5e221f8465f82a3ff563bed39e0
Piotrek <p.sztefko@gmail.com>
2021-06-15 15:59:35 +0200

    Added address controller

---
+++ AddressBook/Controllers/AddressesController.cs
@@ -0,0 +1,38 @@
+﻿using AddressBook.Models;
+using AddressBook.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AddressesController : ControllerBase {
+
+		private readonly IAddressRepository _addressRepository;
+
+
+		public AddressesController(IAddressRepository addressRepository) {
+			_addressRepository = addressRepository;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<Address>> GetAddresses() {
+			return await _addressRepository.Get();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Address>> GetAddresses(int id) {
+			return await _addressRepository.Get(id);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Address>>PostAddress([FromBody] Address address) {
+			var newAddress = await _addressRepository.Create(address);
+			return CreatedAtAction(nameof(GetAddresses), new { id = newAddress.Id }, newAddress);
+		}
+	}
+}
